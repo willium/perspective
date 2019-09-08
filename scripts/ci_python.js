d1b275c786b80f9c67ac99b4e5b926604e807667
Tim Paine <t.paine154@gmail.com>
2019-09-07 20:57:45 -0400

    install optionals

--- scripts/ci_python.js
+++ scripts/ci_python.js
@@ -39,6 +39,7 @@ try {
     let cmd;
     let build_cmd =
         "python3 -m pip install -r requirements.txt &&\
+        python3 -m pip install pytest pytest-cov flake8 codecov pylantern numpy scipy pandas matplotlib &&\
         python3 setup.py build &&\
         python3 -m flake8 perspective && echo OK &&\
         python3 -m pytest -v perspective --cov=perspective";
